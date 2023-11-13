655bed92936e1aae509371633becaf4ee7e03a21
Anette Carlisle <anettecarlisle@gmail.com>
2023-11-13 14:23:18 -0600

    Initial Commit

---
+++ big time charlie/big_time_charlieApp.swift
@@ -0,0 +1,17 @@
+//
+//  big_time_charlieApp.swift
+//  big time charlie
+//
+//  Created by Anette Carlisle on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct big_time_charlieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
