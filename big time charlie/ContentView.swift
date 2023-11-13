1f99667428c6fb50c38e8bb3be73b32f3cef62e9
Anette Carlisle <anettecarlisle@gmail.com>
2023-11-13 14:43:16 -0600

    Added a bird.

--- big time charlie/ContentView.swift
+++ big time charlie/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "bird")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.blue)
+           
+    
+            Text("Hello, bird!")
         }
         .padding()
     }
